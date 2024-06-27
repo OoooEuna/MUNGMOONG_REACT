4eb30a76b2bd8ac8be1c208e1e144bb1d4f8bcfe
ltw0781 <ltw0781@naver.com>
2024-06-27 12:10:24 +0900

    2024-06-27

---
+++ client/src/apis/board.js
@@ -0,0 +1,17 @@
+// 💻 npm install axios
+import axios from 'axios';
+
+// 목록
+export const list = () => axios.get("/boards")
+
+// 조회
+export const select = (no) => axios.get(`/boards/${no}`)
+
+// 등록
+export const insert = (title, writer, content) => axios.post("/boards", {title, writer, content})
+
+// 수정
+export const update = (no, title, writer, content) => axios.put("/boards", {no,title,writer,content})
+
+// 삭제
+export const remove = (no) => axios.delete(`/boards/${no}`)
---
+++ client/src/components/board/List.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const List = ({ boardList, isLoading }) => {
+    console.log(boardList);
+    return (
+        <div className='container'>
+            <h1>게시글 목록</h1>
+            <Link to="/board/insert">글쓰기</Link>
+
+            {
+                isLoading &&
+                <div>
+                    <img src="/img/loading.webp" alt="loading" width="100%" />
+                </div>
+            }
+            {
+                !isLoading && boardList && (
+                    <table border={1}>
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th>작성자</th>
+                                <th>등록일자</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {boardList.map((board) =>
+                            (
+                                <tr key={board.no}>
+                                    <td>{board.no}</td>
+                                    <td>
+                                        <Link to={`/board/${board.no}`}>
+                                            {board.title}
+                                        </Link>
+                                    </td>
+                                    <td>{board.writer}</td>
+                                    <td>{board.regDate}</td>
+                                </tr>
+                            )
+                            )}
+                        </tbody>
+                    </table>
+                )
+            }
+        </div>
+    )
+}
+
+export default List
