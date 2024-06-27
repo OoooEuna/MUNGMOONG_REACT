4b34f9ab4aa6f14ecaf9ba552029d85391445803
OoooEuna <160719463+OoooEuna@users.noreply.github.com>
2024-06-27 16:37:22 +0900

    일단..

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
+      <Route path="/trainer/info/:userId" component={TrainerInfo} />
      
     </Routes>
   </BrowserRouter>
