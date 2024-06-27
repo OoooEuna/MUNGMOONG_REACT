5b8c65d393020d300a7058244ff127b67bf4892a
OoooEuna <160719463+OoooEuna@users.noreply.github.com>
2024-06-27 17:00:27 +0900

    은아

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,27 @@ import Home from './pages/Home';
 import List from './pages/board/List';
 import Read from './pages/board/Read';
 import Update from './pages/board/Update';
+import TrainerInfo from './components/trainer/TrainerInfo';
 
 function App() {
   return (
     <BrowserRouter>
+    <div className='App'>
+    <Header />
     <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="/api/trainer/info/:userId" component={TrainerInfo} />
+      <Route path="/api/trainer/info/:userId" component={TrainerInfo}></Route>
+      {/* <Route path="/api/login" element={ <LoginPage/> }></Route> */}
       <Route path="/api/board" element={ <List/> }></Route>
       <Route path="/api/board/:no" element={ <Read/> }></Route>
       <Route path="/api/board/update/:no" element={ <Update/> }></Route>
      
     </Routes>
+    <Footer />
+    </div>
   </BrowserRouter>
   );
 }
 
 export default App;
+
