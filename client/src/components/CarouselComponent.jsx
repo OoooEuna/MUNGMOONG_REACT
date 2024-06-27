f028dfb28664f7fe5be617a1253121d330174be1
JTNewY <niner2@naver.com>
2024-06-27 10:41:51 +0900

    main 만드는중

---
+++ client/src/components/CarouselComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const CarouselComponent = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/main/slide_01.png" alt="Slide 1" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/main/slide_02.png" alt="Slide 2" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/main/slide_03.png" alt="Slide 3" />
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default CarouselComponent;
