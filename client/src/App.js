83ab4e800b32014baaca37891d6931f841040a9f
JTNewY <niner2@naver.com>
2024-06-27 13:29:57 +0900

    점심 헤더 푸터 푸쉬

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 import Home from './pages/Home';
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-     
-    </Routes>
-  </BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* 다른 라우트들을 정의 */}
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
