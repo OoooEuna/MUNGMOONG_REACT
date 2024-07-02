ad5a06eb9fab55eeede84907dd6b6197c10975cc
wogml909 <zz909@naver.com>
2024-07-02 17:52:12 +0900

    [재희]product수정중

--- client/src/App.js
+++ client/src/App.js
@@ -11,33 +11,29 @@ import LoginPage from './pages/users/LoginPage';
 import LoginContextProvider from './contexts/LoginContextProvider';
 import RegisterPage from './pages/users/RegisterPage';
 import './css/font.css';
+import ProductsList from './pages/products/ProductsList'
 
 
 function App() {
   return (
     <BrowserRouter>
-<LoginContextProvider>
-    <div className='App'>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/api/trainer/info/" element={ <InfoList/> }></Route>
-      {/* <Route path="/api/products" element={ <ProductsList/> }></Route> */}
-      {/* <Route path="/api/login" element={ <LoginPage/> }></Route> */}
-      <Route path="/api/board" element={ <List/> }></Route>
-      <Route path="/api/board/:no" element={ <Read/> }></Route>
-      {/* <Route path="/api/board/update/:no" element={ <Update/> }></Route> */}
-      {/* <Route path="/api/products" element={ <ProductsList/> }></Route> */}
-      <Route path="/api/login" element={ <LoginPage/> }></Route>
-      <Route path="/api/register" element={ <RegisterPage/> }></Route>
-      <Route path="/api/board" element={ <List/> }></Route>
-      <Route path="/api/board/:no" element={ <Read/> }></Route>
-      {/* <Route path="/api/board/update/:no" element={ <Update/> }></Route> */}
-
-    </Routes>
-    <Footer />
-    </div>
-    </LoginContextProvider>
+      <LoginContextProvider>
+          <div className='App'>
+              <Header />
+                  <Routes>
+                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="/api/trainer/info/" element={ <InfoList/> }></Route>
+                    <Route path="/api/products" element={ <ProductsList/> }></Route>
+                    {/* <Route path="/products/:id" element={<ProductsReadContainer />} /> */}
+                    <Route path="/api/login" element={ <LoginPage/> }></Route>
+                    <Route path="/api/register" element={ <RegisterPage/> }></Route>
+                    <Route path="/api/board" element={ <List/> }></Route>
+                    <Route path="/api/board/:no" element={ <Read/> }></Route>
+                    {/* <Route path="/api/board/update/:no" element={ <Update/> }></Route> */}
+                  </Routes>
+              <Footer />
+          </div>
+      </LoginContextProvider>
   </BrowserRouter>
 
   );
