55b88c96a07d0de2e632244bb13320925bac8d7a
Khjoooon <9575khj@naver.com>
2024-07-02 12:05:04 +0900

    QnA 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ import Home from './pages/Home';
 import List from './pages/board/List';
 import Read from './pages/board/Read';
 import Update from './pages/board/Update';
+import Insert from './pages/board/Insert';
 import TrainerInfo from './components/trainer/TrainerInfo';
 // QnA 관련 경로 추가
-// import QnAInsert from './pages/qna/QnAInsert';
-// import QnAList from './pages/qna/QnAList';
-// import QnARead from './pages/qna/QnARead';
-// import QnAUpdate from './pages/qna/QnAUpdate';
+import QnAInsert from './pages/qna/QnAInsert';
+import QnAList from './pages/qna/QnAList';
+import QnARead from './pages/qna/QnARead';
+import QnAUpdate from './pages/qna/QnAUpdate';
 
 function App() {
   return (
@@ -24,13 +25,14 @@ function App() {
       <Route path="/api/trainer/info/:userId" component={TrainerInfo}></Route>
       {/* <Route path="/api/login" element={ <LoginPage/> }></Route> */}
       <Route path="/api/board" element={ <List/> }></Route>
+      <Route path="/api/board/insert" element={ <Insert/> }></Route>
       <Route path="/api/board/:no" element={ <Read/> }></Route>
       <Route path="/api/board/update/:no" element={ <Update/> }></Route>
       {/* QnA 관련 경로 추가 */}
-      {/* <Route path='/api/qna/list' element={<QnAList />} />
-      <Route path='/api/qna/read/:no' element={<QnARead />} />
-      <Route path='/api/qna/update/:no' element={<QnAUpdate />} />
-      <Route path='/api/qna/insert' element={<QnAInsert />} /> */}
+      <Route path='/qna/list' element={<QnAList />} />
+      <Route path='/qna/read/:no' element={<QnARead />} />
+      <Route path='/qna/update/:no' element={<QnAUpdate />} />
+      <Route path='/qna/insert' element={<QnAInsert />} />
 
     </Routes>
     <Footer />
--- client/src/pages/qna/QnAUpdate.jsx
+++ client/src/pages/qna/QnAUpdate.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
+import QnAUpdateContainer from '../../containers/qna/QnAUpdateContainer';
+import { useParams } from 'react-router-dom';
 
 const QnAUpdate = () => {
-    const { no } = useParams()
-    return (
-      <>
-        {/* Header */}
-        <UpdateContainer no={no} />
-        {/* Footer */}
-      </>
-    )
-  }
-  
-  export default QnAUpdate
+  const { no } = useParams();
+  return (
+    <>
+      <QnAUpdateContainer no={no} />
+    </>
+  );
+};
+
+export default QnAUpdate;
