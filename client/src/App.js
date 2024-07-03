57497a8bddf0f207b000cb05a7c1d7642cf6a3d7
Khjoooon <9575khj@naver.com>
2024-07-03 17:45:18 +0900

    마이페이지 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,10 @@ import QnAInsert from './pages/qna/QnAInsert';
 import QnAList from './pages/qna/QnAList';
 import QnARead from './pages/qna/QnARead';
 import QnAUpdate from './pages/qna/QnAUpdate';
+// MyPage 경로 추가
+import UserIndex from './pages/users/Index'; 
+import UpdatePage from './pages/users/UpdatePage';
+
 
 function App() {
   return (
@@ -34,6 +38,11 @@ function App() {
             <Route path='/api/qna/read/:no' element={<QnARead />} />
             <Route path='/api/qna/update/:no' element={<QnAUpdate />} />
             <Route path='/api/qna/insert' element={<QnAInsert />} />
+            {/* MyPage 관련 경로 추가 */}
+            <Route path='/api/users' element={<UserIndex />} />
+            <Route path='/api/users/index' element={<UserIndex />} />
+            <Route path="/api/users/update" element={<UpdatePage />} />
+        
           </Routes>
           <Footer />
         </div>
